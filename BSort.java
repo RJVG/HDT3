f5da1ec328e95980c08e6ed35fa958f798fa843d
KevinMunoz19 <mun15358@uvg.edu.gt>
2016-07-31 10:34:31 -0600

    Bubble sort

---
+++ BSort.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author kevin
+ */
+public class BSort {
+    
+    public static void BSort(String[] args) {
+    int x = 0;
+    int[] array = new int[x];
+    int n = array.length;
+    int temporal;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 1; j < (n - i); j++) {
+
+            if (array[j - 1] > array[j]) {
+                temporal = array[j - 1];
+                array[j - 1] = array[j];
+                array[j] = temporal;
+            }
+
+        }
+    }
+}
+}
